ba8685339af50cf4a8ab06ed5ee320126949cdcd
jesse-kaufman <jesse@jessekaufman.com>
2025-03-30 17:36:06 -0500

    feat(test): add placeholder tests for submit controller unit test and submit API integration test

---
+++ __tests__/integration/submitApi.test.js
@@ -0,0 +1,8 @@
+/** @file Submission API integration tests. */
+import { describe, expect, it } from "vitest"
+
+describe("Submission API integration", () => {
+  it("should be true", () => {
+    expect(true).toBe(true)
+  })
+})
---
+++ __tests__/unit/submitController.test.js
@@ -0,0 +1,8 @@
+/** @file Submission controller unit tests. */
+import { describe, expect, it } from "vitest"
+
+describe("Submit API endpoint", () => {
+  it("should be true", () => {
+    expect(true).toBe(true)
+  })
+})
