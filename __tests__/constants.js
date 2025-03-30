4b14c918dcabd32b8b45d28b6359c7cb027ef5b5
jesse-kaufman <jesse@jessekaufman.com>
2025-03-30 15:43:32 -0500

    feat(test): add constants for use in tests

---
+++ __tests__/constants.js
@@ -0,0 +1,7 @@
+/** @file Constants for tests. */
+export const validSubmission = {
+  name: "John Locke",
+  email: "john.locke@example.com",
+  phone: "3213213214",
+  message: "This is a message.",
+}
