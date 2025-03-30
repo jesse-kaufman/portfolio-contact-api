cdfa4345a849828a7e8287f5ae3e3e6e06cb3909
jesse-kaufman <jesse@jessekaufman.com>
2025-03-30 15:44:24 -0500

    refactor(test): use validSubmission instead of hard-coded objects

--- __tests__/unit/submission.test.js
+++ __tests__/unit/submission.test.js
@@ -1,32 +1,22 @@
 /** @file Unit tests for Submission class. */
 import { describe, it, expect } from "vitest"
+import { validSubmission } from "../constants.js"
 import { Submission } from "@/models/Submission.js"
 
 describe("Submission validation", () => {
   // Test successful call to validate().
   it("should initialize the instance properly if all required fields are provided and valid", () => {
-    const data = {
-      name: "John Doe",
-      email: "john@example.com",
-      phone: "1234567890",
-      message: "This is a message.",
-    }
     let submission = null
 
-    expect(() => (submission = new Submission(data))).not.toThrow()
-    expect(submission.getData()).toEqual(data)
-    expect(submission.toJSON()).toEqual(data)
+    expect(() => (submission = new Submission(validSubmission))).not.toThrow()
+
+    // Check that both toJSON() and getData() return the same object.
+    expect(submission.getData()).toEqual(validSubmission)
+    expect(submission.toJSON()).toEqual(validSubmission)
   })
 
-  it("should throw an error if required fields are missing", () => {
-    expect(
-      () =>
-        new Submission({
-          name: "",
-          email: "",
-          phone: "1234567890",
-          message: "",
-        })
-    ).toThrow("Name, email, and message are required fields.")
+  it("should throw an error if required fields are missing or invalid", () => {
+    expect(() => new Submission({ ...validSubmission, name: "" })).toThrow()
+    expect(() => new Submission({ ...validSubmission, phone: true })).toThrow()
   })
 })
