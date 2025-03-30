e6c6018dccc243d5adace7da2ccd4f03c355eae0
jesse-kaufman <jesse@jessekaufman.com>
2025-03-30 17:13:35 -0500

    chore(test): add mailConfig object to test constants

--- __tests__/constants.js
+++ __tests__/constants.js
@@ -5,3 +5,14 @@ export const validSubmission = {
   phone: "3213213214",
   message: "This is a message.",
 }
+
+/** Valid mail config object. */
+export const mailConfig = {
+  host: process.env.SMTP_SERVER, // Your local mail server address (use 'localhost' for local)
+  port: process.env.SMTP_PORT, // Port for SSL (usually 465 or 587 for TLS)
+  secure: true, // Use SSL/TLS
+  auth: {
+    user: "", // Leave empty if no authentication is required
+    pass: "", // Leave empty if no authentication is required
+  },
+}
